020027b0187e40d4f45a10b789e654a0b98e0c82
Trebilcode <d11a2n3i4e5l6@hotmail.com>
2020-12-15 21:37:16 -0500

    Define routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
-}
+	return (
+		<div>
+			<Router>
+				<div>
+					<Route path='/' exact component={StreamList}></Route>
+					<Route path='/streams/new' exact component={StreamCreate}></Route>
+					<Route path='/streams/edit' exact component={StreamEdit}></Route>
+					<Route path='/streams/delete' exact component={StreamDelete}></Route>
+					<Route path='/streams/show' exact component={StreamShow}></Route>
+				</div>
+			</Router>
+		</div>
+	);
+};
 
 export default App;
